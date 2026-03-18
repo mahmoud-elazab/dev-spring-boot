9a3d09959c0d8bcea88b7d8ace6c28982d2071ae
Mahmoud Elazab <mahmoud.talaat@outlook.de>
2026-03-19 00:48:31 +0100

    Data binding - Text fields

--- 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/melazab/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/melazab/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -3,13 +3,15 @@ package com.melazab.springboot.thymeleafdemo.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
     // a controller method to show the initial HTML form
-    @RequestMapping("/showForm")
+    @GetMapping("/showForm")
     public String showForm(){
         return "helloworld-form";
     }
@@ -43,7 +45,7 @@ public class HelloWorldController {
         return "helloworld";
     }
 
-    @RequestMapping("/processFormv3")
+    @PostMapping("/processFormv3")
     public String shoutV3(@RequestParam("studentName") String theName,
                           Model model){
 
