2e38602f1b61ca936f14a49cf373d3989fe55c63
Mahmoud Elazab <mahmoud.talaat@outlook.de>
2026-03-20 00:48:29 +0100

    Validate a number range

--- 06-spring-boot-spring-mvc/02-validationdemo/src/main/java/com/melazab/mvc/Customer.java
+++ 06-spring-boot-spring-mvc/02-validationdemo/src/main/java/com/melazab/mvc/Customer.java
@@ -1,5 +1,7 @@
 package com.melazab.mvc;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -9,6 +11,10 @@ public class Customer {
     @NotNull(message = "Last Name is required")
     @Size(min = 1, message = "Size must be at least 1 character")
     private  String lastName;
+
+    @Min(value = 1, message = "you must have at least 1 module")
+    @Max(value = 5, message = "you must have maximum 5 modules")
+    private int modules;
 
     public String getFirstName() {
         return firstName;
@@ -24,5 +30,13 @@ public class Customer {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public int getModules() {
+        return modules;
+    }
+
+    public void setModules(int modules) {
+        this.modules = modules;
     }
 }
