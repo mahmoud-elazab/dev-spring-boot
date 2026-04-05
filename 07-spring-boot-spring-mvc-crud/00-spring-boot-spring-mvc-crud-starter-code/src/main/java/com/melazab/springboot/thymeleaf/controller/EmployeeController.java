bc33a83eea11b56a9ea93edf93eab023102b70a2
Mahmoud Elazab <mahmoud.talaat@outlook.de>
2026-04-05 14:49:17 +0200

    adding update employee feature

--- 07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/melazab/springboot/thymeleaf/controller/EmployeeController.java
+++ 07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/melazab/springboot/thymeleaf/controller/EmployeeController.java
@@ -4,10 +4,7 @@ import com.melazab.springboot.thymeleaf.entity.Employee;
 import com.melazab.springboot.thymeleaf.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -34,6 +31,7 @@ public class EmployeeController {
 
     }
 
+
     @GetMapping("/showFormForAdd")
     public String showFormForAdd(Model theModel){
         // create model attribute to bind the form data
@@ -44,6 +42,17 @@ public class EmployeeController {
         return "employees/employee-form";
     }
 
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("employeeId") int theId, Model theModel){
+        // get the employee from the service
+        Employee theEmployee = employeeService.findById(theId);
+
+        // set employee in the model to prepopulate the form
+        theModel.addAttribute("employee", theEmployee);
+
+        // send over to the form
+        return "employees/employee-form";
+    }
     @PostMapping("/save")
     public String saveEmployee(@ModelAttribute("employee") Employee theEmployee){
         // save the employee
