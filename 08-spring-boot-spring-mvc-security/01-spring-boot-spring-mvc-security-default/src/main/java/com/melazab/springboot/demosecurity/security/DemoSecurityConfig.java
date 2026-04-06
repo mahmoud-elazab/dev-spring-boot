5c3de07acd546814af9fb80dcd45071c0e68634d
Mahmoud Elazab <mahmoud.talaat@outlook.de>
2026-04-06 14:19:33 +0200

    adding logout button

--- 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/melazab/springboot/demosecurity/security/DemoSecurityConfig.java
+++ 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/melazab/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -44,7 +44,7 @@ public class DemoSecurityConfig {
                                 .loginPage("/showMyLoginPage")
                                 .loginProcessingUrl("/authenticateTheUser")
                                 .permitAll()
-                );
+                ).logout(logout -> logout.permitAll());
         return http.build();
     }
 
