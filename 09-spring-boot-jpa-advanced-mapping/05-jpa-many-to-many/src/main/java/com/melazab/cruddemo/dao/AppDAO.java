b196825d2908cfdb52dbc68a8b9c38eed6be9c95
Mahmoud Elazab <mahmoud.talaat@outlook.de>
2026-04-11 22:09:44 +0200

    ManyToMany mapping

---
+++ 09-spring-boot-jpa-advanced-mapping/05-jpa-many-to-many/src/main/java/com/melazab/cruddemo/dao/AppDAO.java
@@ -0,0 +1,24 @@
+package com.melazab.cruddemo.dao;
+
+import com.melazab.cruddemo.entity.Course;
+import com.melazab.cruddemo.entity.Instructor;
+import com.melazab.cruddemo.entity.InstructorDetail;
+
+import java.util.List;
+
+public interface AppDAO {
+    void save(Instructor theInstructor);
+    Instructor findInstructorById (int theId);
+    void deleteInstructorById(int theId);
+    InstructorDetail findInstructorDetailById(int theId);
+    void deleteInstructorDetailById(int theId);
+    List<Course> findCoursesByInstructorId(int theId);
+    Instructor findInstructorByIdJoinFetch(int theId);
+    void update(Instructor tempInstructor);
+    void update(Course tempCourse);
+    Course findCourseById(int theId);
+    void deleteCourseById(int theId);
+    void save(Course theCourse);
+    Course findCourseAndReviewsByCourseId(int theId);
+
+}
