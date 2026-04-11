63c14422c520049ef207520406d63b2128dd718c
Mahmoud Elazab <mahmoud.talaat@outlook.de>
2026-04-11 18:57:55 +0200

    OneToMany Uni-directional

---
+++ 09-spring-boot-jpa-advanced-mapping/04-jpa-one-to-many-uni/src/main/java/com/melazab/cruddemo/entity/Course.java
@@ -0,0 +1,96 @@
+package com.melazab.cruddemo.entity;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "course")
+public class Course {
+    // define fields
+
+    // define constructors
+
+    // define getter/setter
+
+    // create toString
+
+    // annotate fields
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "title")
+    private String title;
+
+    @ManyToOne(cascade = {CascadeType.PERSIST,CascadeType.DETACH,CascadeType.MERGE,CascadeType.REFRESH})
+    @JoinColumn(name = "instructor_id")
+    private Instructor instructor;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "course_id")
+    private List<Review> reviews;
+
+    public Course(){
+
+    }
+
+    public Course(String title) {
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Course setId(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Course setTitle(String title) {
+        this.title = title;
+        return this;
+    }
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public Course setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+        return this;
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }
+
+    // add a convenience method
+
+    public void addReview(Review theReview){
+        if (reviews == null){
+            reviews = new ArrayList<>();
+        }
+        reviews.add(theReview);
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                '}';
+    }
+}
